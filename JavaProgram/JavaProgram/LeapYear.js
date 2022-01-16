8ca4fe0ed4479e78bfb0131d671a71c8d46096b7
sayalikeche <kechesayali11@gmail.com>
2022-01-16 22:02:40 +0530

    [sayali Adding Java Programms]

---
+++ JavaProgram/JavaProgram/LeapYear.js
@@ -0,0 +1,5 @@
+﻿var readline = require("readline-sync");
+var year = readline.question("Enter a year");
+if (year % 400 === 0 || (year % 4 === 0 && year % 100 != 0))
+    console.log("It is a leap year");
+else console.log("It is not a leap year");
